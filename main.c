79ef3d7cfb9ed696c2ce70283efc4784b86270c1
Drazzull <arthur.bilibio92@gmail.com>
2015-05-25 20:43:35 -0300

    Criação do menu

--- main.c
+++ main.c
@@ -13,7 +13,38 @@ void pesquisar();
 
 int main(void)
 {
-    printf("Hello World!\n");
+    while(1)
+    {
+        int opcao = 0;
+        printf("Selecione uma opção:\n");
+        printf("1 - Construir árvore genealógica.\n");
+        printf("2 - Salvar\n");
+        printf("3 - Abrir\n");
+        printf("4 - Mostrar\n");
+        printf("5 - Pesquisar\n");
+        printf("6 - Sair\n");
+
+        switch(opcao)
+        {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            return 0;
+        default:
+            printf("Opção inválida");
+            break;
+        }
+    }
     return 0;
 }
 
