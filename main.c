d6e2da77070bc3d23afa7dc6bef4f18fb00172a5
Drazzull <arthur.bilibio92@gmail.com>
2015-06-01 19:26:55 -0300

    Correção dos acentos

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+// Estrutura com as informações necessárias para cada pessoa
 typedef struct sPessoa{
     char nome[100];
     struct sPessoa *pai;
     struct sPessoa *mae;
 } PESSOA;
 
-int numeroReg = 15;
+// Array com as pessoas
 PESSOA arvore[15];
 
+///
+/// \brief construirArvore - Método utilizado para construção da árvore genealógica
+///
 void construirArvore();
 void salvar();
 void abrir();
@@ -21,8 +25,8 @@ int main(void)
     while(1)
     {
         int opcao = 0;
-        printf("Selecione uma opção:\n");
-        printf("1 - Construir árvore genealógica.\n");
+        printf("Selecione uma opcao:\n");
+        printf("1 - Construir arvore genealogica.\n");
         printf("2 - Salvar\n");
         printf("3 - Abrir\n");
         printf("4 - Mostrar\n");
@@ -46,7 +50,7 @@ int main(void)
         case 6:
             return 0;
         default:
-            printf("Opção inválida");
+            printf("Opcao invalida");
             break;
         }
     }
@@ -83,46 +87,46 @@ void construirArvore()
     printf("Informe seu nome: ");
     inserirRegistro(0, 0, 'F');
 
-    printf("Informe o nome da sua mãe: ");
+    printf("Informe o nome da sua mae: ");
     inserirRegistro(1, 0, 'M');
 
     printf("Informe o nome do seu pai: ");
     inserirRegistro(2, 0, 'P');
 
-    printf("Informe o nome da avó materna: ");
+    printf("Informe o nome da avo materna: ");
     inserirRegistro(3, 1, 'M');
 
-    printf("Informe o nome do avô materno: ");
+    printf("Informe o nome do avo materno: ");
     inserirRegistro(4, 1, 'P');
 
-    printf("Informe o nome da avó paterna: ");
+    printf("Informe o nome da avo paterna: ");
     inserirRegistro(5, 2, 'M');
 
-    printf("Informe o nome do avô paterno: ");
+    printf("Informe o nome do avo paterno: ");
     inserirRegistro(6, 2, 'P');
 
-    printf("Informe o nome da mãe da avó materna: ");
+    printf("Informe o nome da mae da avo materna: ");
     inserirRegistro(7, 3, 'M');
 
-    printf("Informe o nome do pai da avó materna: ");
+    printf("Informe o nome do pai da avo materna: ");
     inserirRegistro(8, 3, 'P');
 
-    printf("Informe o nome da mãe do avô materno: ");
+    printf("Informe o nome da mae do avo materno: ");
     inserirRegistro(9, 4, 'M');
 
-    printf("Informe o nome do pai do avô materno: ");
+    printf("Informe o nome do pai do avo materno: ");
     inserirRegistro(10, 4, 'P');
 
-    printf("Informe o nome da mãe da avó paterna: ");
+    printf("Informe o nome da mae da avo paterna: ");
     inserirRegistro(11, 5, 'M');
 
-    printf("Informe o nome do pai da avó paterna: ");
+    printf("Informe o nome do pai da avo paterna: ");
     inserirRegistro(12, 5, 'P');
 
-    printf("Informe o nome da mãe do avô paterno: ");
+    printf("Informe o nome da mae do avo paterno: ");
     inserirRegistro(13, 6, 'M');
 
-    printf("Informe o nome do pai do avô paterno: ");
+    printf("Informe o nome do pai do avo paterno: ");
     inserirRegistro(14, 6, 'P');
 }
 
